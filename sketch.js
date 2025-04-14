2049028db2ada9a4c07a8b7fe2c777eaedb72927
Leon <ly592611595@gmail.com>
2025-04-14 21:32:54 +0800

    feat：change share link

--- sketch.js
+++ sketch.js
@@ -193,7 +193,7 @@ function setup() {
     const msg = `In refining ${macrodataFile.coordinates} (${macrodataFile.fileName}) in ${timeStr} I have brought glory to the company.
 Praise Kier.
 ${thenumbers}#mdrlumon #severance 🧇🐐🔢💯
-TODO: lumon-industries.com`;
+lumonindustries.fun`;
 
     // if (navigator.share) {
     //   console.log("using navigator share");
